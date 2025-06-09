40e4d74d91b9cc5a12a9092147545cf06637aa4b
msoja-pwr <273762@student.pwr.edu.pl>
2025-06-09 19:10:58 +0200

    Add files via upload

---
+++ .github/AED-functionapp/GetAEDPoints/__init__.py
@@ -0,0 +1,30 @@
+import azure.functions as func
+import pymongo
+import os
+import json
+from azure.identity import DefaultAzureCredential
+from azure.keyvault.secrets import SecretClient
+
+def main(req: func.HttpRequest) -> func.HttpResponse:
+    credential = DefaultAzureCredential()
+    key_vault_uri = os.environ["KEYVAULT_URI"]
+    secret_client = SecretClient(vault_url=key_vault_uri, credential=credential)
+    cosmos_conn_str = secret_client.get_secret("cosmosdb-conn-string").value
+
+    client = pymongo.MongoClient(cosmos_conn_str)
+    db = client["AEDdb"]
+    collection = db["AEDpoints"]
+
+    features = []
+    for doc in collection.find():
+        features.append({
+            "type": "Feature",
+            "geometry": doc["geometry"],
+            "properties": doc.get("properties", {})
+        })
+
+    return func.HttpResponse(
+        json.dumps({ "type": "FeatureCollection", "features": features }),
+        mimetype="application/json",
+        status_code=200
+    )
